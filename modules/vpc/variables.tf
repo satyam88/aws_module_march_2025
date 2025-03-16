882844f6223c283a5e8d39047a167e58317aec34
cloudanddevops88 <satyampandey88@gmail.com>
2025-03-16 09:22:07 +0530

    vpc

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,16 @@
+variable "vpc_cidr" {}
+variable "vpc_name" {
+  default = "MyVPC"
+}
+variable "public_subnet_cidrs" {
+  type = list(string)
+}
+variable "private_subnet_cidrs" {
+  type = list(string)
+}
+variable "availability_zones" {
+  type = list(string)
+}
+variable "region" {
+  default = "us-east-1"
+}
